23bc29d27219daaf6fbad15294b1e02382eab2ae
simoneprado83 <si.prado83@gmail.com>
2025-08-28 01:15:55 -0300

    modified:   package-lock.json modified:   package.json modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// troque pelo nome do SEU repositório no GitHub
+const repoName = "simoneprado83-patch-93803";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: `/${repoName}/`, // 👈 base correta para GitHub Pages
+});
