a6c52c7f74528c57403947dfc55786d20afb89c5
simoneprado83 <si.prado83@gmail.com>
2025-08-29 19:49:58 -0300

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // ⚠️ base precisa ser o nome do repositório
 export default defineConfig({
   plugins: [react()],
-  base: '/simoneprado83-patch-93803/docs/',
+  base: '/simoneprado83-patch-93803/',
 })
 
