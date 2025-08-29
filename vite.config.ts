acf1c4cc12f59b686de55bae26093d029e5dbdba
simoneprado83 <si.prado83@gmail.com>
2025-08-29 20:13:07 -0300

    modified:   tsconfig.json modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚠️ base precisa ser o nome do repositório
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/simoneprado83-patch-93803/',
+  base: '/simoneprado83-patch-93803/',  
 })
 
