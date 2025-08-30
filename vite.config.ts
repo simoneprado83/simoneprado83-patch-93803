bbe7ef53600dcf5bf1a0079aa680519d1374532a
simoneprado83 <si.prado83@gmail.com>
2025-08-29 21:41:56 -0300

    Updates

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/simoneprado83-patch-93803/',  
-})
-
