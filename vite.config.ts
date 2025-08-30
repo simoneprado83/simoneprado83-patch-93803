9d498fb9ba2760ce7584ecc87475668fc262dc76
simoneprado83 <si.prado83@gmail.com>
2025-08-30 17:54:16 -0300

    deleted:    vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // base: '/simoneprado83-patch-93803/', // ✨ Comente esta linha
-  plugins: [react()],
-})
