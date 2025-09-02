508e11285488bdee64f6e0cecf742ac2f0e51096
simoneprado83 <si.prado83@gmail.com>
2025-09-01 21:33:27 -0300

    modified:   src/components/BugDetective/BugDetective.tsx

--- src/components/BugDetective/BugDetective.tsx
+++ src/components/BugDetective/BugDetective.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import  { useState, useEffect, useCallback } from "react";
 import styles from "./BugDetective.module.css";
 import { Challenge } from "./types";
 
