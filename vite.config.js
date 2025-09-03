ca04a088e6157e100fa29276ffd21e0903058579
simoneprado83 <si.prado83@gmail.com>
2025-09-03 20:12:27 -0300

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/simoneprado83-patch-93803/',
+  base: '/',
 })
